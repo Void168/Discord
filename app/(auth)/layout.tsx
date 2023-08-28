0e4bbd84cf484ab604621378821ddad592b6bc95
LPHung <phuhung16820@gmail.com>
2023-08-28 16:18:45 +0700

    authentication by clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-rose-200 h-full">{children}</div>;
+  return (
+    <div className="h-full flex items-center justify-center">
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
