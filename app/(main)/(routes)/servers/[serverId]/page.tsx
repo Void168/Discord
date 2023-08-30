73cddb26be135b2b084eac5c9d371167d6377355
LPHung <phuhung16820@gmail.com>
2023-08-30 16:48:16 +0700

    create navigation side bar

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,9 @@
+interface ServerPageProps {
+    
+}
+ 
+const ServerPage: React.FunctionComponent<ServerPageProps> = () => {
+    return ( <div>Server Id Page</div> );
+}
+ 
+export default ServerPage;
