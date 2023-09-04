fa34376b4c7fe7bb35fa5fbeb169c77215738c65
LPHung <phuhung16820@gmail.com>
2023-09-04 14:22:01 +0700

    display list of members to manage members modal

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -10,6 +10,15 @@ import {
   DialogDescription,
 } from "@/components/ui/dialog";
 import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className='h-4 w-4 ml-2 text-indigo-500' />,
+  ADMIN: <ShieldAlert className='h-4 w-4 text-rose-500' />,
+};
 
 const MembersModal = () => {
   const { onOpen, isOpen, onClose, type, data } = useModal();
@@ -22,7 +31,7 @@ const MembersModal = () => {
       open={isModalOpen}
       onOpenChange={onClose}
     >
-      <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+      <DialogContent className='bg-white text-black overflow-hidden'>
         <DialogHeader className='pt-8 px-6'>
           <DialogTitle className='text-2xl text-center font-bold'>
             Invite Friends
@@ -31,7 +40,25 @@ const MembersModal = () => {
             {server?.members?.length} Members
           </DialogDescription>
         </DialogHeader>
-        <div className='p-6'>Members</div>
+        <ScrollArea className='mt-8 max-h-[420px] pr-6'>
+          {server?.members?.map((member) => (
+            <div
+              key={member.id}
+              className='flex items-center gap-x-2 mb-6'
+            >
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className='flex flex-col gap-y-1'>
+                <div className='text-xs font-semibold flex items-center gap-1'>
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">
+                  {member.profile.email}
+                </p>
+              </div>
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
