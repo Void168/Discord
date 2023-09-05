c38e12c001225f92182e7c3394028f4b7c12029d
LPHung <phuhung16820@gmail.com>
2023-09-05 16:55:01 +0700

    display delete server modal UI

---
+++ components/modals/delete-server-modal.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useModal } from "@/hooks/use-modal-store";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+const DeleteServerModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+  const router = useRouter()
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { server } = data;
+  const isModalOpen = isOpen && type === "deleteServer";
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true)
+
+      await axios.patch(`/api/servers/${server?.id}/leave`)
+      onClose()
+      router.refresh()
+      router.push('/')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  return (
+    <Dialog
+      open={isModalOpen}
+      onOpenChange={onClose}
+    >
+      <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+        <DialogHeader className='pt-8 px-6'>
+          <DialogTitle className='text-2xl text-center font-bold'>
+            Delete Server
+          </DialogTitle>
+          <DialogDescription className='text-center text-zinc-500'>
+            Are you sure want to delete this{" "}
+            <span className='font-semibold text-indigo-500'>
+              {server?.name}
+            </span>{" "}
+            server?
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <div className='flex items-center justify-between w-full px-6 pb-6'>
+            <Button
+              disabled={isLoading}
+              onClick={onClose}
+              variant='ghost'
+            >
+              Cancel
+            </Button>
+            <Button
+              disabled={isLoading}
+              onClick={onClick}
+              variant='primary'
+            >
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeleteServerModal;
--- components/providers/modal-providers.tsx
+++ components/providers/modal-providers.tsx
@@ -8,6 +8,7 @@ import EditServerModal from "@/components/modals/edit-server-modal";
 import MembersModal from "@/components/modals/members-modal";
 import CreateChannelModal from "@/components/modals/create-channel-modal";
 import LeaveServerModal from "@/components/modals/leave-server-modal";
+import DeleteServerModal from "@/components/modals/delete-server-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -27,6 +28,7 @@ const ModalProvider = () => {
       <MembersModal />
       <CreateChannelModal />
       <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
