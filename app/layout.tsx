5d5b38ce50161462808ceacfc3b2b5376f1f18fb
LPHung <phuhung16820@gmail.com>
2023-09-11 15:26:54 +0700

    create socket provider to apply to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import ModalProvider from "@/components/providers/modal-providers";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -21,19 +22,25 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+      <html
+        lang='en'
+        suppressHydrationWarning
+      >
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
+            attribute='class'
+            defaultTheme='dark'
             enableSystem={false}
-            storageKey="discord-theme"
+            storageKey='discord-theme'
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
   );
 }
+
