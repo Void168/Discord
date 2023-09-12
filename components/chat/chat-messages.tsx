f307f100c33ab51459b7e98842531e4a77d8da9f
LPHung <phuhung16820@gmail.com>
2023-09-12 15:56:34 +0700

    create chat-messages component

---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Member } from "@prisma/client"
+
+interface ChatMessagesProps {
+    name: string
+    member: Member
+    chatId: string
+    apiUrl: string
+    socketUrl: string
+    socketQuery: Record<string, string>
+    paramKey: "channelId" | "conversationId"
+    paramValue: string
+    type: "channel" | "conversation"
+}
+
+export const ChatMessages = ({
+  name,
+  member,
+  chatId,
+  apiUrl,
+  socketUrl,
+  socketQuery,
+  paramKey,
+  paramValue,
+  type,
+}: ChatMessagesProps) => {
+  return <div>Chat Messages</div>;
+};
