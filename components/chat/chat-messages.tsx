be4430635f61d4ab8c89d97fa2db0550c26bde98
LPHung <phuhung16820@gmail.com>
2023-09-12 16:55:35 +0700

    display messages in chat component

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import { Member } from "@prisma/client";
+import { Member, Message, Profile } from "@prisma/client";
 import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
+import { Fragment } from "react";
+
+type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
+};
 
 interface ChatMessagesProps {
   name: string;
@@ -47,18 +54,18 @@ export const ChatMessages = ({
         </p>
       </div>
     );
-    }
-    
-    if (status === "error") {
-      return (
-        <div className='flex flex-col flex-1 justify-center items-center'>
-          <ServerCrash className='h-7 w-7 text-zinc-500 my-4' />
-          <p className='text-xs text-zinc-500 dark:text-zinc-400'>
-            Something went wrong!
-          </p>
-        </div>
-      );
-    }
+  }
+
+  if (status === "error") {
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <ServerCrash className='h-7 w-7 text-zinc-500 my-4' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
   return (
     <div className='flex-1 flex flex-col py-4 overflow-y-auto'>
       <div className='flex-1'>
@@ -66,6 +73,15 @@ export const ChatMessages = ({
           type={type}
           name={name}
         />
+        <div className='flex flex-col-reverse mt-auto'>
+          {data?.pages?.map((group, i) => (
+            <Fragment key={i}>
+              {group.items.map((message: MessageWithMemberWithProfile) => (
+                <div key={message.id}>{message.content}</div>
+              ))}
+            </Fragment>
+          ))}
+        </div>
       </div>
     </div>
   );
