1fc7363326ac9099fd5f88a9e62798aef72ccfdd
LPHung <phuhung16820@gmail.com>
2023-09-12 18:37:31 +0700

    styled chat item

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,7 +1,10 @@
-import { Member, Profile } from "@prisma/client";
+import { Member, MemberRole, Profile } from "@prisma/client";
 import { UserAvatar } from "../user-avatar";
 import { ActionTooltip } from "../action-tooltip";
-import { ShieldAlert, ShieldCheck } from "lucide-react";
+import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 interface ChatItemProps {
   id: string;
@@ -36,6 +39,19 @@ export const ChatItem = ({
   socketUrl,
   socketQuery,
 }: ChatItemProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const fileType = fileUrl?.split(".").pop();
+
+  const isAdmin = currentMember.role === MemberRole.ADMIN;
+  const isModerator = currentMember.role === MemberRole.MODERATOR;
+  const isOwner = currentMember.id === member.id;
+  const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+  const canEditMessage = !deleted && isOwner && !fileUrl;
+  const isPDF = fileType === "pdf" && fileUrl;
+  const isImage = !isPDF && fileUrl;
+
   return (
     <div className='relative-group flex items-center hover:bg-black/5 p-4 transition w-full'>
       <div className='group flex gap-x-2 items-start w-full'>
@@ -52,13 +68,74 @@ export const ChatItem = ({
                 {roleIconMap[member.role]}
               </ActionTooltip>
             </div>
-            <span className="text-xs text-zinc-500 dark:text-zinc-400">{timestamp}</span>
-                  </div>
-                  {
-                      content
-                  }
+            <span className='text-xs text-zinc-500 dark:text-zinc-400'>
+              {timestamp}
+            </span>
+          </div>
+          {isImage && (
+            <a
+              href={fileUrl}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48'
+            >
+              <Image
+                src={fileUrl}
+                alt={content}
+                fill
+                className='object-cover'
+              />
+            </a>
+          )}
+          {isPDF && (
+            <div className='relative flex items-center p-2 mt-2 rounded-md bg-background/10'>
+              <FileIcon className='h-10 w-10 fill-indigo-200 stroke-indigo-400' />
+              <a
+                href={fileUrl}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline'
+              >
+                PDF File
+              </a>
+            </div>
+          )}
+          {!fileUrl && !isEditing && (
+            <p
+              className={cn(
+                "text-sm text-zinc-600 dark:text-zinc-300",
+                deleted &&
+                  "italic text-zinc-500 dark:text-zinc-400 text-xs mt-1"
+              )}
+            >
+              {content}
+              {isUpdated && !deleted && (
+                <span className='text-[10px] mx-2 text-zinc-500 dark:text-zinc-400'>
+                  (edited)
+                </span>
+              )}
+            </p>
+          )}
         </div>
       </div>
+      {canDeleteMessage && (
+        <div className='flex items-center gap-x-2 p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm'>
+          {canEditMessage && (
+            <ActionTooltip label='Edit'>
+              <Edit
+                onClick={() => setIsEditing(true)}
+                className='cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition'
+              />
+            </ActionTooltip>
+          )}
+          <ActionTooltip label='Delete'>
+            <Trash
+              onClick={() => {}}
+              className='cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition'
+            />
+          </ActionTooltip>
+        </div>
+      )}
     </div>
   );
 };
